13ae5dbf1c4bf4c8362ff8cf8aec768d1f74a8e5
Sophie3lj <sophie.3lj@gmail.com>
2022-01-22 02:34:07 +0000

    created gui base and meme mode

---
+++ web_gui/public/index.js
@@ -0,0 +1,16 @@
+
+var inMemeMode = false;
+
+function memeMode() {
+    var body = document.getElementById("body");
+    var logo = document.getElementById("logo");
+    if (!inMemeMode){
+        body.classList.add("meme-mode");
+        logo.src = "MemeLogo.jpeg";
+        inMemeMode = true;
+    } else {
+        body.classList.remove("meme-mode");
+        logo.src = "Logo.png";
+        inMemeMode = false;
+    }
+}
