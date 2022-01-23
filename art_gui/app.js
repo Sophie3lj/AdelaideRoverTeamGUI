fcfd6e59db6fc6a1948bfcf076693f1a240ad916
abisparnon <abisparnon@gmail.com>
2022-01-23 11:13:47 +1030

    new folder

---
+++ art_gui/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var ws = require('ws');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+var client = new ws('ws://129.127.30.45:9090');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
